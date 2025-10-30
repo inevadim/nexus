8df743389c81b03752c03866fb0a72c1289dfcff
Ostap Poliakov <elcortezos@gmail.com>
2025-10-30 21:48:20 +0300

    Adjust shadcn styles, tailwind, add button component

--- src/components/desk/Desk.tsx
+++ src/components/desk/Desk.tsx
@@ -1,51 +1,54 @@
 import styles from "./Desk.module.scss"
 import { Widget } from "./widget/Widget"
-import { AddWidget } from "./addWidget/AddWidget";
-import { useDispatch, useSelector } from "react-redux";
-import type { RootState } from "../../app/model/store";
-import { changeVisible } from "../../app/model/features/modal/modalSlice";
-import { Modal } from "../modal/Modal";
-import { addWidget, removeWidget } from "../../app/model/features/widgets/widgetsSlice";
-
-
-
+import { AddWidget } from "./addWidget/AddWidget"
+import { useDispatch, useSelector } from "react-redux"
+import type { RootState } from "@/app/model/store"
+import { changeVisible } from "@/app/model/features/modal/modalSlice"
+import { Modal } from "../modal/Modal"
+import { addWidget, removeWidget } from "@/app/model/features/widgets/widgetsSlice"
+import { Button } from "@/components/ui/button"
 
 export const Desk = () => {
-    const modal = useSelector((state: RootState) => state.modal.value)
-    const widgets = useSelector((state: RootState) => state.widgets.value)
-    const dispatch = useDispatch()
+  const modal = useSelector((state: RootState) => state.modal.value)
+  const widgets = useSelector((state: RootState) => state.widgets.value)
+  const dispatch = useDispatch()
 
-    const handleOpenModal = () => {
-        dispatch(changeVisible()) 
-    }
+  const handleOpenModal = () => {
+    dispatch(changeVisible())
+  }
 
-    const handleAddWidgetConfirmed = (type: string, component: React.ComponentType) => {
-        dispatch(addWidget({ type, component }))
-    }
+  const handleAddWidgetConfirmed = (type: string, component: React.ComponentType) => {
+    dispatch(addWidget({ type, component }))
+  }
 
-    const handleRemoveWidget = (id: string) => { 
-        dispatch(removeWidget(id))
-    }
+  const handleRemoveWidget = (id: string) => {
+    dispatch(removeWidget(id))
+  }
 
-    return(
-        <div className={styles.wrapDesk}>
-            <div className={styles.desk}>
-                {widgets.map(widget => (
-                    <Widget 
-                        key={widget.id}
-                        type={widget.type}
-                        component={widget.component}
-                        onRemove={() => handleRemoveWidget(widget.id)}
-                    />
-                ))}
-                
-                {widgets.length >= 0 && widgets.length < 8 && (
-                    <div onClick={handleOpenModal}>
-                        <AddWidget />
-                    </div>
-                )}
-            </div>
-            {modal && <Modal onAddWidget={handleAddWidgetConfirmed} />}
+  return (
+    <div className={styles.wrapDesk}>
+      <div className={styles.desk}>
+        {widgets.map(widget => (
+          <Widget
+            key={widget.id}
+            type={widget.type}
+            component={widget.component}
+            onRemove={() => handleRemoveWidget(widget.id)}
+          />
+        ))}
+
+        {widgets.length >= 0 && widgets.length < 8 && (
+          <div onClick={handleOpenModal}>
+            <AddWidget />
+          </div>
+        )}
+        <div className='flex flex-row items-center justify-center gap-5'>
+          <Button>Default</Button>
+          <Button variant={"secondary"}>Secondary</Button>
+          <Button variant={"ghost"}>Ghost</Button>
         </div>
-    )
+      </div>
+      {modal && <Modal onAddWidget={handleAddWidgetConfirmed} />}
+    </div>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client"
 import App from "./App.tsx"
 import { store } from "./app/model/store.ts"
 import { Provider } from "react-redux"
+import "./styles/index.css"
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
